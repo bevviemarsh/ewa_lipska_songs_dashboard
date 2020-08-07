2232aa4a85c0530314d9108ef69301cfb0a3cbcc
Paweł Marciniak <pablomchod@gmail.com>
2020-08-07 20:32:52 +0200

    Updates

---
+++ precache-manifest.72276310e8416c1387aefba9a00421cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b79618597bec163292ff4d7fd054457",
+    "url": "/ewa_lipska_songs_dashboard/index.html"
+  },
+  {
+    "revision": "80454055493e5c87e89c",
+    "url": "/ewa_lipska_songs_dashboard/static/css/main.e24d2b7c.chunk.css"
+  },
+  {
+    "revision": "e0663f06f63869cba5d1",
+    "url": "/ewa_lipska_songs_dashboard/static/js/2.a0539d79.chunk.js"
+  },
+  {
+    "revision": "2903f65e346887768e2d109a63fdd7a4",
+    "url": "/ewa_lipska_songs_dashboard/static/js/2.a0539d79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80454055493e5c87e89c",
+    "url": "/ewa_lipska_songs_dashboard/static/js/main.0c97fcfd.chunk.js"
+  },
+  {
+    "revision": "93069cbac6fa2d65ea99",
+    "url": "/ewa_lipska_songs_dashboard/static/js/runtime-main.15a6a446.js"
+  }
+]);
